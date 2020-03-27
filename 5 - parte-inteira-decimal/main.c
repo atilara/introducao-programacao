d8dd4a0f4f1ea82db7c1cd957f125de927528ba0
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-03-27 12:43:20 -0300

    Quinta questão

---
+++ 5 - parte-inteira-decimal/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	float numero, parteDecimal;
+	int parteInteira;
+  	printf("Informe o número: ");
+	scanf("%f", &numero);
+	parteInteira = numero;
+	parteDecimal = numero - parteInteira;
+	printf("Inteira: %i \nDecimal: %.2f", parteInteira, parteDecimal);
+  	return 0;
+}
