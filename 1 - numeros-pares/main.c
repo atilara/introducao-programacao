82137e70ede7e2ec4652f73618304ccd6641e13a
Jeremias Oliveira da Silva <38990510+Jeremias333@users.noreply.github.com>
2020-03-29 16:59:40 -0300

    update file.c
    comentado

--- 1 - numeros-pares/main.c
+++ 1 - numeros-pares/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+//cade o comentário meu filho?
 int main(void) {
+	// criando a variavel de incremento
   	int i;
+	//fazendo a iteração pelo for enquanto é percorrido
 	for (i = 1; i<=1000; i++) {
+		//no if compara se o i é par.
 		if (i%2 == 0) {
+			//printa na tela o que passou pela seleção.
 			printf("%i\n", i);
 		}
 	}
