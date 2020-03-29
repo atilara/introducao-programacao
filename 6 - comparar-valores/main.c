71a978332d7c9c91889c8ef2900a28af6f39ff5b
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-03-29 16:56:16 -0300

    Atualizando sexta questão
    Finalizando a questão

--- 6 - comparar-valores/main.c
+++ 6 - comparar-valores/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-// Questão incompleta
 int main(void) {
 
-	int valores[3];
+	int valores[2];
 	int i, maiorValor, menorValor;
-
-	for(i = 1; i<=3; i++) {
-		printf("Informe o %iº valor: ", i);
+	for(i = 0; i<=2; i++) {
+		printf("Informe o %iº valor: ", i+1);
 		scanf("%i", &valores[i]);
-
+	}
+	maiorValor = valores[0];
+	menorValor = valores[0];
+	for(i = 0; i<=2; i++) {
 		if (valores[i] < menorValor) {
 			menorValor = valores[i];
 		} else if (valores[i] > maiorValor) {
@@ -17,5 +18,14 @@ int main(void) {
 		}
 	}
 
-	printf("\nO menor valor é: %i\nO maior é valor é: %i", menorValor, maiorValor);
+	printf("\nO menor valor é: %i\nO maior é valor é: %i\n", menorValor, maiorValor);
+	if (menorValor < 0) {
+		printf("\nO menor em módulo vale: %i", menorValor * -1);
+	} else if (menorValor >= 0) {
+		printf("\nO menor em módulo vale ele mesmo");
+	} if (maiorValor < 0) {
+		printf("\nO maior valor em módulo vale: %i", maiorValor * -1);
+	} else if (maiorValor >= 0) {
+		printf("\nO maior valor em módulo vale ele mesmo");
+	}
 }
