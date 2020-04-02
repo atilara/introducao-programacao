84d9de689066f61925db8be465fae7459dca4902
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-04-02 15:58:25 -0300

    Adicionando comentário, 13ª questão, lista 1

--- primeira-lista/13 - ano-bissexto/main.c
+++ primeira-lista/13 - ano-bissexto/main.c
@@ -4,6 +4,9 @@ int main(void) {
 	int ano;
 	printf("Informe o valor do ano: ");
 	scanf("%i", &ano);
+	// verifica matematicamente se o ano é bissexto, caso seja divisível por 400, 
+	// mesmo que seja divisível por 100, será bissexto, caso não seja divísivel por 400, mas seja por 100
+	// não será bissexto.
 	if ((ano % 400 == 0 || ano % 100 != 0) && ano % 4 == 0) {
 		printf("É bissexto");
 	} else {
