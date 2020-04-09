9bc7d8549ab21c126ce72176845b7cb83e8dd61d
Átila Rodrigues <atila.rodriguesal@gmail.com>
2020-04-09 19:58:38 -0300

    Atualizar primeira lista, décima segunda questão
    Melhor resolução utilizando vetores

--- primeira-lista/12 - interseccao-circulos/main.c
+++ primeira-lista/12 - interseccao-circulos/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-    float x1, y1, r1, x2, y2, r2, pitagoras;
-    printf("Informe a posição do primeiro círculo no eixo x:");
-    scanf("%f", &x1);
-    printf("Informe a posição do primeiro círculo no eixo y:");
-    scanf("%f", &y1);
-    printf("Informe o raio do primeiro círculo: ");
-    scanf("%f", &r1);
-    printf("Informe a posição do segundo círculo no eixo x:");
-    scanf("%f", &x2);
-    printf("Informe a posição do segundo círculo no eixo y:");
-    scanf("%f", &y2);
-    printf("Informe o raio do segundo círculo: ");
-    scanf("%f", &r2);
-    pitagoras = ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
-    if (((r1+r2)*(r1+r2)) >= pitagoras) {
+    float x[2], y[2], r[2], pitagoras;
+    int i;
+    // Recebe as informações necessárias para o cálculo em seus respectivos vetores utilizando um loop, reduzindo a quantidade de linhas utilizadas no código anterior
+    for (i = 0; i < 2; i++) {
+		printf("Informe a posição do %iº círculo no eixo x:", i+1);
+		scanf("%f", &x[i]);
+	    printf("Informe a posição do %iº círculo no eixo y:", i+1);
+		scanf("%f", &y[i]);
+		printf("Informe o raio do %iº círculo: ", i+1);
+    	scanf("%f", &r[i]);
+	}
+    // realiza o cálculo do lado direito da inequação utilizando uma variável de apoio, o resto do cálculo é feito no if statement
+    pitagoras = ((x[1]-x[0])*(x[1]-x[0])) + ((y[1]-y[0])*(y[1]-y[0]));
+    if (((r[0]+r[1])*(r[0]+r[1])) >= pitagoras) {
         printf("Existe uma intersecção");
     } else {
         printf("Não existe uma intersecção");
